5fd688bd63df273a8c64043ad2bafb8c70721b17
AmarS2001 <63029750+AmarS2001@users.noreply.github.com>
2021-11-11 22:54:36 +0530

    importing basic necessary libraries for the project

---
+++ proj_file1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+from pyspark.sql import SparkSession
+from sklearn.preprocessing import StandardScalar
+######################################################
+

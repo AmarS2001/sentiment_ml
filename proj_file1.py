5b32edaa8b61fa9f80d32104c9eee2f26e503bca
AmarS2001 <63029750+AmarS2001@users.noreply.github.com>
2021-11-18 05:46:47 +0530

    To view and convert the json streaming data to dataframe.
    Firstly the connection is made to the host and socket of the streaming server (stream.py). The streamed rdd is converted to json format and json is then converted to dataframe.

--- proj_file1.py
+++ proj_file1.py
@@ -3,6 +3,26 @@ import pandas as pd
 import json
 import matplotlib.pyplot as plt
 from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
 from sklearn.preprocessing import StandardScalar
+import time
 ######################################################
 
+sc=SparkContext("local","ml_spark")
+sc.setLogLevel('OFF')
+ssc=StreamingContext(sc,1)
+sp=SparkSession(sc)
+
+def output(rdd):
+  df=sp.read.json(rdd)
+  print(df)
+  #df.printSchema()
+  #df.show()
+  
+
+lines=ssc.socketTextStream("locahost",6100)
+lines.foreachRDD(lambda rdd:output(rdd))
+
+ssc.start()
+ssc.awaitTermination()

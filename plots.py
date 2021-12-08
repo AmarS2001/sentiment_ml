28dd200b1520174bb9108340a0deef2921fcf20c
AmarS2001 <63029750+AmarS2001@users.noreply.github.com>
2021-12-08 18:31:02 +0530

    final_update

--- plots.py
+++ plots.py
@@ -1,22 +1,40 @@
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df=pd.read_csv("train_track.csv")
-x=np.arange(len(df))
-y1=np.array(df['SVM_class'])
-y2=np.array(df['NB_Class'])
-y3=np.array(df['RF_class'])
+if sys.argv[1] == 'test':
+	df=pd.read_csv("train_track.csv")
+	x=np.arange(len(df))
+	y1=np.array(df['SVM_class'])
+	y2=np.array(df['NB_Class'])
+	y3=np.array(df['RF_class'])
 
-plt.plot(x,y1)
-plt.plot(x,y2)
-plt.plot(x,y3)
+	plt.plot(x,y1)
+	plt.plot(x,y2)
+	plt.plot(x,y3)
 
-plt.xlabel('batches ->')  
-plt.ylabel('Score ->')
+	plt.xlabel('batches ->')  
+	plt.ylabel('Score ->')
 
-plt.legend(['SVM', 'Naive Bias', 'Random Forest'])
+	plt.legend(['SVM', 'Naive Bias', 'Random Forest'])
 
-plt.show()
+	plt.show()
+
+else:
+	df2= pd.read_csv("cluster_track.csv")
+	x=np.arange(len(df2))
+	y1=np.array(df2['centroid1_diff'])
+	y2=np.array(df2['centroid2_diff'])
+	
+	plt.plot(x,y1)
+	plt.plot(x,y2)
+	
+	plt.xlabel('batches ->')  
+	plt.ylabel('centroid_shift ->')
+
+	plt.legend(['centroid_shift 1', 'centroid_shift 2'])
+
+	plt.show()
 
